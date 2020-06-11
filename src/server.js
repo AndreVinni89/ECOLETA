8b978aa842f78e7171a99f19d4a620cfc8d2191b
AndreVinni89 <andrevinnicius89@gmail.com>
2020-06-11 11:53:38 -0300

    dia5 - part 1

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const server = express()
 
+
+///Importar o banco de dados
+const db = require("./database/db.js")
+
+
+
 //Tornando a pasta public invisivel
 server.use(express.static("public"))
 
@@ -19,12 +25,41 @@ server.get("/", (req, res) => {
     return res.render("index.html")
 })
 
+
+
+
+
 server.get("/create-point", (req, res) => {
+    
+    
+    
+    
     return res.render("create-point.html")
+
 })
 
+
+
+
+
+
+
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html")
+
+    //Puxando os dados do banco
+    db.all(`
+        SELECT * FROM places`, function (err, rows) {//rows para retornar uma lista com os registros
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Here are your registers: ")
+        console.log(rows)
+
+        const total = rows.length 
+
+
+        return res.render("search-results.html", {places : rows, total: total})
+    })    
 })
 
 
